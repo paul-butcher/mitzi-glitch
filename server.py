0cae679722ef20405518695e113726f6905f5dca
Glitch (mitzi-del-bra) <none>
2020-10-10 21:40:27 +0000

    🍚🚪 Updated with Glitch

---
+++ server.py
@@ -0,0 +1,22 @@
+from io import BytesIO
+from flask import Flask
+from flask import request
+from mitzi import houndstooth
+
+app = Flask(__name__)
+
+
+@app.route("/houndstooth.png")
+def get_image():
+    buffer = BytesIO()
+
+    img = houndstooth(request.args['c1'], request.args['c2'], 6, (512, 512))
+    img.save(buffer, "PNG")
+
+    response = app.make_response(buffer.getvalue())
+    response.mimetype = "image/png"
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
